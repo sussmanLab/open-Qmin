89ce14893ab2e53213ff996c01632ee8fbaf8bb7
Daniel Sussman <dmsussman@gmail.com>
2018-05-30 18:18:47 -0400

    nudge

--- src/forces/baseLatticeForce.cu
+++ src/forces/baseLatticeForce.cu
@@ -19,6 +19,7 @@ __global__ void gpu_lattice_spin_force_nn_kernel(dVec *d_force,
     if(zeroForce)
         d_force[idx] = make_dVec(0.0);
     int3 target = latticeIndex.inverseIndex(idx);
+    /*
     int smx,spx,smy,spy,smz,spz;
     smx = latticeIndex(wrap(target.x-1,L),target.y,target.z);
     spx = latticeIndex(wrap(target.x+1,L),target.y,target.z);
@@ -26,17 +27,23 @@ __global__ void gpu_lattice_spin_force_nn_kernel(dVec *d_force,
     spy = latticeIndex(target.x,wrap(target.y+1,L),target.z);
     smz = latticeIndex(target.x,target.y,wrap(target.z-1,L));
     spz = latticeIndex(target.x,target.y,wrap(target.z+1,L));
-
     if(zeroForce)
         d_force[idx] = J*(d_spins[smx]+d_spins[spx] + d_spins[smy]+d_spins[spy] + d_spins[smz]+d_spins[spz]) ;
     else
         d_force[idx] += J*(d_spins[smx]+d_spins[spx] + d_spins[smy]+d_spins[spy] + d_spins[smz]+d_spins[spz]) ;
-
-//    d_force[idx] += J*d_spins[spx];
-//    d_force[idx] += J*(d_spins[smy]+d_spins[spy]);
-//    d_force[idx] += J*d_spins[spy];
-//    d_force[idx] += J*(d_spins[smz]+d_spins[spz]);
-//    d_force[idx] += J*d_spins[spz];
+    */
+    dVec smx,spx,smy,spy,smz,spz,ans;
+    smx = d_spins[latticeIndex(wrap(target.x-1,L),target.y,target.z)];
+    spx = d_spins[latticeIndex(wrap(target.x+1,L),target.y,target.z)];
+    smy = d_spins[latticeIndex(target.x,wrap(target.y-1,L),target.z)];
+    spy = d_spins[latticeIndex(target.x,wrap(target.y+1,L),target.z)];
+    smz = d_spins[latticeIndex(target.x,target.y,wrap(target.z-1,L))];
+    spz = d_spins[latticeIndex(target.x,target.y,wrap(target.z+1,L))];
+    ans = J*(smx+smy+smz+spx+spy+spz);
+    if(zeroForce)
+        d_force[idx] = ans ;
+    else
+        d_force[idx] += ans ;
     }
 
 bool gpu_lattice_spin_force_nn(dVec *d_force,
