1a9ae65da7f0ca50364e11ea2ee1deb730e8ab7c
Daniel <dmsussman@gmail.com>
2019-04-01 14:24:10 -0400

    removed qt53d dependencies

--- cubicLatticeNematic.cpp
+++ cubicLatticeNematic.cpp
@@ -5,15 +5,15 @@
 #include <QTimer>
 #include <QGuiApplication>
 
-#include <Qt3DCore/QEntity>
-#include <Qt3DRender/QCamera>
-#include <Qt3DRender/QCameraLens>
-#include <Qt3DCore/QTransform>
-#include <Qt3DCore/QAspectEngine>
+//#include <Qt3DCore/QEntity>
+//#include <Qt3DRender/QCamera>
+//#include <Qt3DRender/QCameraLens>
+//#include <Qt3DCore/QTransform>
+//#include <Qt3DCore/QAspectEngine>
 
-#include <Qt3DInput/QInputAspect>
+//#include <Qt3DInput/QInputAspect>
 
-#include <Qt3DRender/QRenderAspect>
+//#include <Qt3DRender/QRenderAspect>
 //#include <Qt3DExtras/Qt3DWindow>
 //#include <Qt3DExtras/QForwardRenderer>
 //#include <Qt3DExtras/QPhongMaterial>
